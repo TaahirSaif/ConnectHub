868a91ff94bf981d5fd1d59f8cb914de41749c54
TaahirSaif <88341523+TaahirSaif@users.noreply.github.com>
2024-12-20 08:09:47 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const airtimeRoutes = require('./routes/airtime');
+const analyticsRoutes = require('./routes/analytics');
+const { dbConnect } = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+dbConnect();
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/airtime', airtimeRoutes);
+app.use('/api/analytics', analyticsRoutes);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
